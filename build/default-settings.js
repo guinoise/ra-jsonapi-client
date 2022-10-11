df74a1d1537618380d46d8439e457e734100f39e
Eric Guinois <eric.guinois@gatas.ca>
2022-10-10 21:33:29 -0400

    build

---
+++ build/default-settings.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  total: 'total',
+  headers: {
+    Accept: 'application/vnd.api+json; charset=utf-8',
+    'Content-Type': 'application/vnd.api+json; charset=utf-8'
+  },
+  updateMethod: 'PATCH',
+  arrayFormat: 'brackets',
+  getManyKey: 'id'
+};
+exports["default"] = _default;
